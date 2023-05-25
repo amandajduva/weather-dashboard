47203caab835359f796c8e4dc49bbf94e85d3b45
Amanda Duva <amanda.j.duva@gmail.com>
2023-05-24 22:30:41 -0400

    updated current weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
-// let searchHistory = [];
+let searchHistory = [];
 let apiKey = "6d1d20fdf7cdd2f943896cd1e2c397b5";
-// let apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
-// icons
-// let iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
 
 let searchForm = document.querySelector("#search-form");
 let searchInput = document.querySelector("#search-input");
@@ -12,18 +9,18 @@ let forecast = document.querySelector("#forecast");
 
 function searchCity(event) {
     event.preventDefault();
-    
-    // we need to capture the user input --> input element value
+
+    // capture the user input --> input element value
     console.log("Event: ", event)
     console.log("Target: ", event.target)
     console.log("Value: ", searchInput.value);
     let cityName = searchInput.value
-    if(cityName === '') {
+    if (cityName === '') {
         return;
     }
     // either CALL or PASS the city name to our FETCH method/function
-    
-    let geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+
+    let geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
     fetch(geoUrl)
         .then(response => {
             console.log("Response: ", response)
@@ -33,54 +30,135 @@ function searchCity(event) {
             console.log("Data: ", data);
             let lat = data[0].lat;
             let lon = data[0].lon;
-            
+
             console.log("Coord: ", lat, lon);
 
-            // If we want to make an second or third or fouth ... API call using the data we gathered. We have to do it inside the function scope
-
             // PASS the values on to the function being called
-            currentWeather(lat, lon);
+            currentWeather(lat, lon, cityName);
+            forecastWeather(lat, lon, cityName);
+            // let cityList = localStorage.getItem();
+            // if (cityList) {
+
+            // }
+            if (searchHistory.indexOf(cityName) === -1) {
+                searchHistory.push(cityName);
+                localStorage.setItem("cityList", JSON.stringify(searchHistory));
+                showSearchHistory();
+            }
         })
         .catch(error => {
             throw error;
         });
-        
-    }
-    
-function currentWeather(lat, lon) {
-    
-    //let apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${name}&units=imperial`;
+
+}
+
+function currentWeather(lat, lon, cityName) {
+
     let currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    
+
     fetch(currentWeatherAPI)
         .then(response => response.json())
         .then(data => {
             console.log("Current Weather Data: ", data);
 
-            let temp = data.main.temp;
+            let temp = ((data.main.temp - 273.15) * 9) / 5 + 32;
+            temp = temp.toFixed(2);
+            let wind = data.wind.speed;
+            let humidity = data.main.humidity;
+            let currentDay = dayjs();
+            console.log(currentDay);
+            currentDay = currentDay.format("MM/DD/YYYY");
 
             // Create new elements and APPEND them to the DOM 
-            let tempElement = document.createElement('h2');
-            tempElement.textContent = temp;
+            let displayHeading = document.createElement("h2");
+            displayHeading.setAttribute("id", "title");
+            let iconImg = document.createElement("img");
+            let iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            iconImg.setAttribute("src", iconUrl);
+
+            displayHeading.textContent = cityName + " (" + currentDay + ") ";
+            today.append(displayHeading);
+            displayHeading.append(iconImg);
+
+
+            let tempElement = document.createElement("p");
+            tempElement.textContent = "Temperature: " + temp + "°F";
             today.append(tempElement);
+
+            let windElement = document.createElement("p");
+            windElement.textContent = "Wind: " + wind + " mph";
+            today.append(windElement);
+
+            let humElement = document.createElement("p");
+            humElement.textContent = "Humidity: " + humidity + "%";
+            today.append(humElement);
         })
         .catch(error => {
             throw error;
         });
 }
 
-function searchHistory() {
-    history.innerHTML = "";
+function forecastWeather(lat, lon, cityName) {
 
-    for (let i = history.length - 1; i >= 0; i--) {
-        let btn = document.createElement("button");
-        btn.setAttribute("type", "button");
-        btn.classList("");
-        btn.setAttribute("", searchHistory[i]);
-        btn.textContent = searchHistory[i];
-        history.append(btn);
-    }
-    searchHistory();
+    let forecastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+    // let forecastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+    fetch(forecastAPI)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Forecast Data: ", data);
+
+            for (i = 0; i < forecast.length; i++) {
+                let forecastIndex = i * 8 + 4;
+
+                // let temp = ;
+                // let wind = ;
+                // let humidity = ;
+
+                // // needs date and icon added
+
+                // let tempElement = document.createElement("p");
+                // tempElement.textContent = "Temperature: " + temp;
+                // today.append(tempElement);
+
+                // let windElement = document.createElement("p");
+                // windElement.textContent = "Wind: " + wind + " mph";
+                // today.append(windElement);
+
+                // let humElement = document.createElement("p");
+                // humElement.textContent = "Humidity: " + humidity + "%";
+                // today.append(humElement);
+            }
+        })
+        .catch(error => {
+            throw error;
+        });
 }
 
-searchForm.addEventListener("submit", searchCity); 
+// function searchHistory() {
+//     history.innerHTML = "";
+
+//     for (let i = history.length - 1; i >= 0; i--) {
+//         let btn = document.createElement("button");
+//         btn.setAttribute("type", "button");
+//         btn.setAttribute("", searchHistory[i]);
+//         btn.textContent = searchHistory[i];
+//         history.append(btn);
+//     }
+//     searchHistory();
+// }
+function showSearchHistory() {
+    let cityList = localStorage.getItem("cityList");
+    if (cityList) {
+        let cityListArr = JSON.parse(cityList)
+        history.innerHTML = "";
+        for (let i = cityListArr.length - 1; i >= 0; i--) {
+            let btn = document.createElement("button");
+            btn.setAttribute("type", "button");
+            // btn.setAttribute("", searchHistory[i]);
+            btn.textContent = searchHistory[i];
+            history.append(btn);
+        }
+    }
+}
+    searchForm.addEventListener("submit", searchCity); 
